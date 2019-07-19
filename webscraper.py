9a8d0fbfb224d5e5d515e2a33811bef06f0bb122
Angus Li <angusli@Anguss-MBP-2.attlocal.net>
2019-07-18 23:56:38 -0700

    got rid of comments

--- webscraper.py
+++ webscraper.py
@@ -24,21 +24,15 @@ while session == True:
 	htmlFile = urlRequest.text
 
 	soup = BeautifulSoup(htmlFile, 'html.parser')
-	#audienceScore = soup.findAll('a', href = '#audience_reviews')
-	#criticScore = soup.findAll('a', href = '#contentReviews')
 
 	temp = soup.findAll('div', class_ = 'mop-ratings-wrap__half')
-	#print(temp)
-	#print("buffer")
+
 	critic = temp[0].findAll('span', class_ = 'mop-ratings-wrap__percentage')
 	print("Critic Score: " + critic[0].text.strip())
 	print("Critic Ratings: " + temp[0].small.text.strip())
-	#print("buffer")
+	
 	audience = temp[1].findAll('span', class_ = 'mop-ratings-wrap__percentage')
 	print("Audience Score: " + audience[0].text.strip())
 	print(temp[1].strong.text.strip())
-
-	#print("Audience Score: " + audienceScore[0].text.strip())
-	#print("Critics Score: " + criticScore[0].text.strip())
 
 
